bd19f0015af579e4df4a6c0c9730222e6670a6e5
Gonzalo <gonzalogomez22@gmail.com>
2021-02-03 13:44:47 -0300

    Primer commit

---
+++ CrudEntityFramework/Migrations/20210125134943_NuevaMigracion.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudEntityFramework.Migrations
+{
+    public partial class NuevaMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Celular",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Telefono",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Nombre",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Celular",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ CrudEntityFramework/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [Display(Name = "Teléfono")]
+        public String Telefono { get; set; }
+
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public String Celular { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public String Email { get; set; }
+    }
+}
